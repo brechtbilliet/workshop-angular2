771ad98c05375886d1377e763608f6adbb7b67e6
Brecht Billiet <billietbrecht@gmail.com>
2016-06-16 17:51:05 +0200

    reducers

--- src/common/reducers/data/authenticationReducer.ts
+++ src/common/reducers/data/authenticationReducer.ts
@@ -1,5 +1,7 @@
 import {Action} from "@ngrx/store";
 import {AuthenticationDataState} from "../../state/DataState";
+import {DATA_AUTHENTICATION_SET_AUTHENTICATION, DATA_AUTHENTICATION_CLEAR_AUTHENTICATION} from "../../actionTypes";
+import {AuthenticationResult} from "../../../authentication/types/AuthenticationResult";
 let authenticationState: AuthenticationDataState = {
     isAuthenticated: false,
     jwtToken: "",
@@ -8,6 +10,23 @@ let authenticationState: AuthenticationDataState = {
 export function authenticationReducer(state: AuthenticationDataState = authenticationState,
                                       action: Action = null): AuthenticationDataState {
     switch (action.type) {
+        case DATA_AUTHENTICATION_SET_AUTHENTICATION:
+            let payload: AuthenticationResult = action.payload;
+            return {
+                isAuthenticated: true,
+                jwtToken: payload.token,
+                account: {
+                    firstName: payload.firstName,
+                    lastName: payload.lastName,
+                    login: payload.login
+                }
+            };
+        case DATA_AUTHENTICATION_CLEAR_AUTHENTICATION:
+            return {
+                isAuthenticated: false,
+                jwtToken: null,
+                account: null
+            };
         default:
             return state;
     }
