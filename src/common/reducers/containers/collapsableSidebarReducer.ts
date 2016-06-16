51dd07bbcc0003be0dd85e7b9b1f40206340df8a
Brecht Billiet <billietbrecht@gmail.com>
2016-06-16 17:55:46 +0200

    removed implementations

--- src/common/reducers/containers/collapsableSidebarReducer.ts
+++ src/common/reducers/containers/collapsableSidebarReducer.ts
@@ -1,14 +1,9 @@
 import {Action} from "@ngrx/store";
 import {CollapsableSidebarContainerState} from "../../state/ContainersState";
-import {CONTAINER_COLLAPSABLESIDEBAR_TOGGLE} from "../../actionTypes";
 export function collapsableSidebarReducer(state: CollapsableSidebarContainerState = {isCollapsed: false},
                                           action: Action = null): CollapsableSidebarContainerState {
     switch (action.type) {
-        case CONTAINER_COLLAPSABLESIDEBAR_TOGGLE:
-            return {
-                isCollapsed: !state.isCollapsed
-            };
         default:
-            return state;
+            return null;
     }
 };
--- src/common/reducers/containers/editStockReducer.ts
+++ src/common/reducers/containers/editStockReducer.ts
@@ -1,17 +1,8 @@
 import {Action} from "@ngrx/store";
 import {EditStockContainerState} from "../../state/ContainersState";
-import {CONTAINER_EDITSTOCKPAGE_SET_WINE, CONTAINER_EDITSTOCKPAGE_CLEAR_WINE} from "../../actionTypes";
 export function editStockReducer(state: EditStockContainerState = {wine: null}, action: Action = null): EditStockContainerState {
     switch (action.type) {
-        case CONTAINER_EDITSTOCKPAGE_SET_WINE:
-            return <EditStockContainerState> Object.assign({}, state, {
-                wine: action.payload
-            });
-        case CONTAINER_EDITSTOCKPAGE_CLEAR_WINE:
-            return <EditStockContainerState> Object.assign({}, state, {
-                wine: null
-            });
         default:
-            return state;
+            return null;
     }
 };
